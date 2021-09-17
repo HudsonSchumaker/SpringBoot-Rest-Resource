78c0958dd0538e7ab3ccc084f1870ce3c1d6952d
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-17 20:28:18 +0200

    Commit.

--- src/main/kotlin/br/com/schumaker/data/model/Console.kt
+++ src/main/kotlin/br/com/schumaker/data/model/Console.kt
@@ -2,9 +2,11 @@ package br.com.schumaker.data.model
 
 import java.util.*
 import javax.persistence.Entity
+import javax.persistence.Id
 
 @Entity
 data class Console(
+    @Id
     val id: String = UUID.randomUUID().toString(),
     val name: String,
     val brand: String,
--- src/main/kotlin/br/com/schumaker/data/model/ConsoleRepository.kt
+++ src/main/kotlin/br/com/schumaker/data/model/ConsoleRepository.kt
@@ -1,10 +1,12 @@
 package br.com.schumaker.data.model
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource
 interface ConsoleRepository: CrudRepository<Console, String> {
 
-
+    fun findByBrand(@Param("brand") brand: String): List<Console>
+    fun findByYear(@Param("year") year: Int): List<Console>
 }
