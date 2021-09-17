58513db2d08d1e2b0592f08ce677f98bdc87f767
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-17 20:06:24 +0200

    Commit.

--- src/main/kotlin/br/com/schumaker/data/model/ConsoleRepository.kt
+++ src/main/kotlin/br/com/schumaker/data/model/ConsoleRepository.kt
@@ -1,10 +1,12 @@
 package br.com.schumaker.data.model
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 
-@RepositoryRestResource
+
 class ConsoleRepository(var consoles: HashMap<String, Console>): CommonRepository<Console> {
 
+    @RestResource(rel = "save")
     override fun save(domain: Console): Console? {
         val console = consoles.get(domain.id)
         if (null != console) {
@@ -24,6 +26,7 @@ class ConsoleRepository(var consoles: HashMap<String, Console>): CommonRepositor
        consoles.remove(domain.id)
     }
 
+    @RestResource(rel = "find")
     override fun findById(id: String): Console {
         return consoles.getOrDefault(id, Console(
             id = "",
