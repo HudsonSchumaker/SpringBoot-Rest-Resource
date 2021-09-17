27e83fcc299c404da5e2cf46dcefaefe8b48e95a
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-17 19:26:06 +0200

    Commit.

---
+++ src/main/kotlin/br/com/schumaker/data/model/Consoles.kt
@@ -0,0 +1,14 @@
+package br.com.schumaker.data.model
+
+import java.util.*
+
+data class Consoles(
+    val id: String = UUID.randomUUID().toString(),
+    val name: String,
+    val brand: String,
+    val year: Int,
+    val cpu: String,
+    val gpu: String,
+    val ram: String,
+    val vram: String
+)
